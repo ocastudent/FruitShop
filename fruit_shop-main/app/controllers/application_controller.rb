794d4074d5cf7e4d76cb667793a7f6c217269b5d
ocastudent <145093236+ocastudent@users.noreply.github.com>
2025-08-29 03:13:47 +0000

    08-29

--- fruit_shop-main/app/controllers/application_controller.rb
+++ fruit_shop-main/app/controllers/application_controller.rb
@@ -21,4 +21,17 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :admin_flg])
   end
+
+  private
+
+  def store_recent_product
+    # 今見ているページが products コントローラーの show アクション（＝商品詳細ページ）のときだけ処理
+    if params[:controller] == 'products' && params[:action] == 'show'
+      product_id = params[:id].to_i  # パラメータから商品IDを取得（文字列→整数に変換）
+      session[:recent_product_ids] ||= []  # セッションに recent_product_ids がなければ空配列で初期化
+      session[:recent_product_ids].delete(product_id)  # 重複を防ぐために、すでにあるIDを削除
+      session[:recent_product_ids].unshift(product_id) # 商品IDを配列の先頭に追加（新しい順に並べる）
+      session[:recent_product_ids] = session[:recent_product_ids].take(5)  # 配列の先頭5件だけを残す（＝最大5件まで表示）
+    end
+  end
 end
